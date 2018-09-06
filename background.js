8d2dcdfd35abf560e200e4e8440ba185b13f0be5
Frank Cusano <frank.cusano9@gmail.com>
2018-09-05 23:09:13 -0400

    Added webview to play stream, probably not the best solution

--- background.js
+++ background.js
@@ -1,9 +1,12 @@
 
 chrome.app.runtime.onLaunched.addListener(function() {
     chrome.app.window.create('window.html', {
-      'outerBounds': {
-        'width': 400,
-        'height': 500
-      }
+      'innerBounds': {
+        'width': 600,
+        'height': 338,
+        'minWidth': 400,
+        'minHeight': 300,
+      },
+      'frame': "none"
     });
-  });
+});
