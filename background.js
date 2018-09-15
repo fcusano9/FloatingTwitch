9ef415c3fab0027b8e39c8dbeb315b51dc55b218
Frank Cusano <frank.cusano9@gmail.com>
2018-09-14 21:29:37 -0400

    Added drag bar to allow frameless window to be dragged

--- background.js
+++ background.js
@@ -1,12 +1,13 @@
 
 chrome.app.runtime.onLaunched.addListener(function() {
-    chrome.app.window.create('window.html', {
-      'innerBounds': {
-        'width': 600,
-        'height': 338,
-        'minWidth': 400,
-        'minHeight': 300,
-      },
-      'frame': "none"
-    });
+	chrome.app.window.create("window.html", {
+		frame: "none",
+		id: "windowId",
+		innerBounds: {
+			"width": 600,
+			"height": 338,
+			"minWidth": 400,
+			"minHeight": 300
+		},
+	});
 });
