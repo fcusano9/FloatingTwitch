6dfec06939393e714cecb20c93b3efd3b2a20510
Frank Cusano <frank.cusano9@gmail.com>
2018-09-21 22:10:57 -0400

    Made window resizable and also maintain aspect ratio of the video

--- window.js
+++ window.js
@@ -1,1 +1,51 @@
+// MIT License
 
+// Copyright (c) 2018 Frank Cusano
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+
+var appWindow = chrome.app.window.current()
+
+appWindow.contentWindow.document.addEventListener("keydown", function(event) {
+	// Close window
+	if (event.keyCode == 88) { // 'x' key
+		appWindow.close();
+	}
+	
+	// Make window larger by 3%
+	if (event.keyCode == 107) { // '+' key
+
+		var newWidth = Math.round(appWindow.innerBounds.width * 1.03),
+			newHeight = Math.round(newWidth * (9 / 16));
+
+		appWindow.innerBounds.width = newWidth;
+		appWindow.innerBounds.height = newHeight;
+	}
+
+	// Make window smaller by 3%
+	if (event.keyCode == 109) { // '-' key
+
+		var newWidth = Math.round(appWindow.innerBounds.width / 1.03),
+		newHeight = Math.round(newWidth * (9 / 16));
+
+		appWindow.innerBounds.width = newWidth;
+		appWindow.innerBounds.height = newHeight;
+	}
+})
