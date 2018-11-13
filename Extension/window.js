60ca8466ad45c5ab7352dd8994e75dab670e22c9
Frank Cusano <frank.cusano9@gmail.com>
2018-11-13 12:51:22 -0500

    Tried to fix some errors in the extension Also added some styling to the input form

--- Extension/window.js
+++ Extension/window.js
@@ -16,3 +16,37 @@ chrome.windows.onCreated.addListener(function() {
 		console.log(url);
 	});
 });
+
+
+
+
+
+
+// Subscribe to click event of the submit button
+document.getElementById("submitButton").addEventListener("click", function(){
+
+	// Get the elements that are needed
+	//var webview = document.getElementById("webview");
+	var inputForm = document.getElementById("inputForm");
+	var channel = document.getElementById("channel");
+	
+	// If the user didn't enter a stream name return
+	// var stream = channel.value;
+	// if (stream === "" || stream === null){
+	// 	return;
+	// }
+
+	// Set the source of the webview to the twitch channel
+	//webview.src = "https://player.twitch.tv/?channel=" + stream + "&muted=false";
+
+	// webview.addEventListener("permissionrequest", function(e) {
+	// 	if (e.permission === "fullscreen") {
+	// 		e.request.allow();
+	// 	}
+	// });
+
+	// Hide the form and show the webview
+	inputForm.style.display = "none";
+
+	//webview.style.display = "initial";
+});
